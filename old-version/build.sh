5ec865104a90389163d118d9a12b21a77c8f3db0
inmail <393894240@qq.com>
2019-10-31 17:04:04 +0800

    old version

--- old-version/build.sh
+++ old-version/build.sh
@@ -7,8 +7,8 @@ BUILD_DIR=${BUILD_DIR:-build}
 BUILD_TYPE=${BUILD_TYPE:-debug}
 CXX=${CXX:-g++}
 
-mkdir -p $BUILD_DIR-$BUILD_TYPE-cpp11 \
-    && cd $BUILD_DIR-$BUILD_TYPE-cpp11 \
+mkdir -p $BUILD_DIR/$BUILD_TYPE-cpp11 \
+    && cd $BUILD_DIR/$BUILD_TYPE-cpp11 \
     && cmake \
              -DCMAKE_BUILD_TYPE=$BUILD_TYPE \
              $SOURCE_DIR \
