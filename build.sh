6bfea855f5cab9a570824ef227f23918b1e0dcfc
inmail <393894240@qq.com>
2019-11-11 20:37:27 +0800

    add rpc

--- build.sh
+++ build.sh
@@ -1,5 +1,8 @@
 #!/bin/sh
 
+#说明：
+#  虽然使用了C++14编译选项，但是除了rpc部分用了少量C++14的特性外，其余均为C++11编写
+
 set -x
 
 SOURCE_DIR=`pwd`
@@ -8,8 +11,8 @@ BUILD_TYPE=${BUILD_TYPE:-debug}
 INSTALL_DIR=${INSTALL_DIR:-../install-${BUILD_TYPE}}
 CXX=${CXX:-g++}
 
-mkdir -p $BUILD_DIR/$BUILD_TYPE-cpp11 \
-	&& cd $BUILD_DIR/$BUILD_TYPE-cpp11 \
+mkdir -p $BUILD_DIR/$BUILD_TYPE-cpp14 \
+	&& cd $BUILD_DIR/$BUILD_TYPE-cpp14 \
 	&& cmake \
 		   -DCMAKE_BUILD_TYPE=$BUILD_TYPE \
 		   -DCMAKE_INSTALL_PREFIX=$INSTALL_DIR \
